e930ec3d8e30d1515403e5e986cd4a9b024199be
Victor Rozhkov <46989161+Lazoozoo@users.noreply.github.com>
2019-04-24 21:15:59 +0300

    Delete 004 Удаление, изменение записей.sql

--- ДЗ по Базам данных/004 Удаление, изменение записей.sql
+++
@@ -1,5 +0,0 @@
-UPDATE client SET ФИО='Найденова Галина Павловна' WHERE ФИО='Герасимова Галина Павловна'
-UPDATE client SET Пасспорт='1234567890' WHERE ФИО='Жук Иван Иванович'
-UPDATE account SET Баланс='0' WHERE idДоговора='1000006'
-DELETE FROM account WHERE id = '10006'
-
